61eb87693a6b8dcb32528e652297496c187c8b74
Patrik Lundgren <patrik.lundgren@outlook.com>
2021-12-16 20:23:15 +0100

    Delegate knowledge of initializing netspace to init script
    Signed-off-by: Patrik Lundgren <patrik.lundgren@outlook.com>

--- .config/netspace/init.sh
+++ .config/netspace/init.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-$START /usr/sbin/dnsmasq -x /run/dnsmasq/dnsmasq.pid -u dnsmasq -7 /etc/dnsmasq.d,.dpkg-dist,.dpkg-old,.dpkg-new --local-service
-$START /usr/sbin/in.tftpd --listen --user tftp --address :69 --secure /tftpboot
+export START="sudo ip netns exec $NETSPACE"
+for i in $(ls $(dirname $0)/init.d); do
+	$i
+done
