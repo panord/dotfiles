9050784c6b6f778dc9b29dfb4044ae4fe08df4b1
Patrik Lundgren <patrik.lundgren@outlook.com>
2021-12-27 10:35:44 +0100

    Add helper script for getting certificates from server
    Signed-off-by: Patrik Lundgren <patrik.lundgren@outlook.com>

---
+++ bin/get-cert.sh
@@ -0,0 +1,40 @@
+target_cert=$(curl-config --ca)
+
+yorn()
+{
+	while true; do
+		read -p "$* [y/N]: " yn
+		case $yn in
+			[Yy]*) return 0;;
+			[Nn]*) return 1;;
+			*) return 0;;
+		esac
+	done
+}
+
+usage()
+{
+	echo "usage: $0 hostname port [target]"
+	echo "example: $0 mydomain.com 443 example.pem"
+}
+
+hostname=$1
+port=$2
+
+if [ -z "$hostname" ] || [ -z "$port" ]; then
+	usage
+	exit 1
+fi
+
+if [ -z "$target_cert" ]; then
+	echo "Could not find default certificate target curl-config --ca"
+	exit 1
+fi
+
+resp=$(echo "^D" | openssl s_client -showcerts -connect $hostname:$port -servername $hostname 2>/dev/null)
+cert=$(echo "$resp" | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p')
+echo -e "Extracting cert from response...\n\tResponse\n${resp}\n\tCert\n${cert}"
+
+if yorn "Save certificate to ${target_cert}?"; then
+	echo "$cert" >> ${target_cert}
+fi
